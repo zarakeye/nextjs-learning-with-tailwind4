188139c3982296ea1d9473ab14d815e473757e47
Stéphane Bazzé <stephane.bazze@outlook.fr>
2025-07-13 18:14:13 +0200

    Pogress in Next.js learning with Tailwind 4

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+export const inter = Inter({
+  subsets: ['latin'],
+});
